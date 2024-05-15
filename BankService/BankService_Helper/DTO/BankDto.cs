42e6bfb65127fbce2a8f83aa7c7a30e780d8cedc
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2024-05-15 11:32:38 +0200

    code refactor

---
+++ BankService/BankService_Helper/DTO/BankDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BankService_Helper.DTO
+{
+    public class BankDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Bic { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+    }
+}
