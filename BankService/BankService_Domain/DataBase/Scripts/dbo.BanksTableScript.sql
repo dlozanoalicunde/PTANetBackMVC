a49d3d8ef037009d8f31d949dfe66436df666aab
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2024-05-15 11:36:34 +0200

    code refactor

---
+++ BankService/BankService_Domain/DataBase/Scripts/dbo.BanksTableScript.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Banks] (
+    [Id]      INT        NOT NULL,
+    [Name]    NCHAR (50) NOT NULL,
+    [Bic]     NCHAR (20) NOT NULL,
+    [Country] NCHAR (2)  DEFAULT ('NO') NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
