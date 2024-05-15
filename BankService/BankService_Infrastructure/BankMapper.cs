8bb7bd35859ad8a2f90bf807cdea129be1522762
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2024-05-15 11:32:23 +0200

    code refactor

---
+++ BankService/BankService_Infrastructure/BankMapper.cs
@@ -0,0 +1,33 @@
+﻿namespace BankService_Infrastructure
+{
+    using BankService_Domain.Models;
+    using BankService_Helper.DTO;
+
+    public static class BankMapper
+    {
+        public static BankDto ToDto(Bank entity)
+        {
+            BankDto dto = new BankDto
+            {
+                Id = entity.Id,
+                Bic = entity.Bic,
+                Country = entity.Country,
+                Name = entity.Name
+            };
+
+            return dto;
+        }
+
+        public static Bank ToEntity(BankDto dto)
+        {
+            Bank bank = new Bank()
+            {
+                Bic = dto.Bic,
+                Country = dto.Country,
+                Name = dto.Name
+            };
+
+            return bank;
+        }
+    }
+}
---
+++ BankService/BankService_Infrastructure/Repositories/BankRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace BankService_Infrastructure.Repositories
+{
+    using System.Threading.Tasks;
+    using BankService_Domain;
+    using BankService_Domain.Models;
+    using BankService_Helper.DTO;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BankRepository : IBankRepository
+    {
+        private readonly BankservicedbMdfContext _dbContext;
+
+        public BankRepository(BankservicedbMdfContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(BankDto bank)
+        {
+            await _dbContext.Banks.AddAsync(BankMapper.ToEntity(bank));
+        }
+
+        public async Task<BankDto?> GetBy(int bankId)
+        {
+            Bank entity = await _dbContext.Banks.SingleAsync(b => b.Id == bankId);
+
+            return entity is not null ? BankMapper.ToDto(entity) : null;
+        }
+    }
+}
