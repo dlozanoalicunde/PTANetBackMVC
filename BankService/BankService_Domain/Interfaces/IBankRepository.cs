7926e71d24ffee9230e95a17864d59bf85b5be06
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2024-05-15 11:33:40 +0200

    code refacotr

---
+++ BankService/BankService_Domain/Interfaces/IBankRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BankService_Domain
+{
+    using BankService_Helper.DTO;
+
+    public interface IBankRepository
+    {
+        Task<BankDto?> GetBy(int bankId);
+
+        Task Add(BankDto bank);
+    }
+}
