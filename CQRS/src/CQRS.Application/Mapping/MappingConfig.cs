a28b68ec7d7c9c98ba98a44765fb97cfb43b19bb
damdavidprieto <dam.david.prieto@gmail.com>
2024-05-25 17:12:36 +0200

    rellenar mapper

--- CQRS/src/CQRS.Application/Mapping/MappingConfig.cs
+++ CQRS/src/CQRS.Application/Mapping/MappingConfig.cs
@@ -1,3 +1,5 @@
+using CQRS.Application.DTOs;
+using CQRS.Domain.Entities;
 using Mapster;
 
 namespace CQRS.Application.Mapping;
@@ -6,6 +8,14 @@ public static class MappingConfig
 {
     public static void Configure()
     {
+        TypeAdapterConfig<Bank, BankDto>.NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Bic, src => src.Bic)
+            .Map(dest => dest.Country, src => src.Country);
 
+        TypeAdapterConfig<BankDto, Bank>.NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Bic, src => src.Bic)
+            .Map(dest => dest.Country, src => src.Country);
     }
 }
