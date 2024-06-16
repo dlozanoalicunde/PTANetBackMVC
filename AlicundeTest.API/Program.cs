deab2adbd5f5d5283c71da7b8b20041b25117cae
ElPavlillo <elpavlillo@gmail.com>
2024-06-16 17:35:15 +0200

    Swagger configuration

--- AlicundeTest.API/Program.cs
+++ AlicundeTest.API/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+// Swagger configuration
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AlicundeTest API",
+        Description = "Alicunde coding test",
+        Version = "v1",
+    });
+
+    // Documentation configuration
+    string? xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    string? xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -11,6 +30,16 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Adding swagger middleware
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlicundeTest API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
 }
 
 app.UseHttpsRedirection();
