ea75e59b13d98f47cea269dab66de088500acb8e
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 18:59:45 +0200

    Created UnitTest project

---
+++ AlicundeTest.UnitTest/Banks/CreateBankTest.cs
@@ -0,0 +1,17 @@
+﻿using AlicundeTest.Domain.Models;
+
+namespace AlicundeTest.UnitTest.Banks;
+
+internal class CreateBankTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CreateBank()
+    {
+        
+    }
+}
