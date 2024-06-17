304442aba68ba6fd0ddaf1fd9030480f3ad32a61
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 18:10:21 +0200

    Added Persistence layer with dbContext

---
+++ AlicundeTest.Persistence/AlicundeTestDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AlicundeTest.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlicundeTest.Persistence;
+
+public class AlicundeTestDbContext : DbContext
+{
+    public AlicundeTestDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Bank> Banks { get; set; }
+}
