9adae9634cb0a56e841f6be5748db6f14fd5a72e
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 18:12:35 +0200

    Added UnitOfWork to persistence layer

---
+++ AlicundeTest.Persistence/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace AlicundeTest.Persistence;
+
+public class UnitOfWork
+{
+    private readonly AlicundeTestDbContext _dbContext;
+
+    public UnitOfWork(AlicundeTestDbContext dbContext) => _dbContext = dbContext;
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default) =>
+        _dbContext.SaveChangesAsync(cancellationToken);
+}
