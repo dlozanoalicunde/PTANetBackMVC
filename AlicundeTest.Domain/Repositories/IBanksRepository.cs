f2fd050fa7a4f150c5c3451feebd1f98879bc388
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 18:18:03 +0200

    Correction in IBankRepository

--- AlicundeTest.Domain/Repositories/IBanksRepository.cs
+++ AlicundeTest.Domain/Repositories/IBanksRepository.cs
@@ -4,6 +4,6 @@ namespace AlicundeTest.Domain.Repositories;
 
 public interface IBanksRepository
 {
-    Task<Bank> GetAll(CancellationToken cancellationToken = default);
-    Task<IEnumerable<Bank>> GetBank(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Bank>> GetAll(CancellationToken cancellationToken = default);
+    Task<Bank> GetBank(Guid id, CancellationToken cancellationToken = default);
 }
