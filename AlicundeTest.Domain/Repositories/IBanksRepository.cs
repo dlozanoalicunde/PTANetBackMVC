1a8843d400b482009ee2396aefa8a4642f79d6de
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 17:45:14 +0200

    Added interfaces for bank repository and unit of work

---
+++ AlicundeTest.Domain/Repositories/IBanksRepository.cs
@@ -0,0 +1,9 @@
+﻿using AlicundeTest.Domain.Models;
+
+namespace AlicundeTest.Domain.Repositories;
+
+public interface IBanksRepository
+{
+    Task<Bank> GetAll(CancellationToken cancellationToken = default);
+    Task<IEnumerable<Bank>> GetBank(Guid id, CancellationToken cancellationToken = default);
+}
