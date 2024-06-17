9f6c9c1f19a47abac81fc75dc49f86cce0123919
ElPavlillo <elpavlillo@gmail.com>
2024-06-17 17:08:44 +0200

    Domain layer with Bank model and abstract entity

---
+++ AlicundeTest.Domain/Models/Bank.cs
@@ -0,0 +1,27 @@
+﻿using AlicundeTest.Domain.Abstract;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlicundeTest.Domain.Models;
+
+public class Bank : Entity<Bank>
+{
+    public Bank() {}
+
+    public Bank(string name, string bic, string country)
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.Now;
+        Name = name;
+        BIC = bic;
+        Country = country;
+    }
+
+    [MaxLength(400)]
+    public string Name { get; set; }
+
+    [MaxLength(11)]
+    public string BIC { get; set; }
+
+    [MaxLength(2)]
+    public string Country { get; set; }
+}
